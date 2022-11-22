d3251407eace3cf3b64a9c405bf571f9622a5518
Anas Zanane <azanane@z1r2p2.42lyon.fr>
2022-11-22 09:29:25 +0100

    CPP06, ready to push.

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -2,27 +2,24 @@
 
 uintptr_t serialize(Data* ptr)
 {
-	// void	*tmpVoid = ptr;
-	// uintptr_t value = reinterpret_cast<uintptr_t &>(tmpVoid);
-
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data* deserialize(uintptr_t raw)
 {
-	// void	*tmpVoid = &raw;
-	// Data *dataDeserialize = &reinterpret_cast<Data &>(tmpVoid);
-
 	return reinterpret_cast<Data *>(raw);
 }
 
 int	main()
 {
-	Data *data = new Data();
+	Data 		*data = new Data();
+	uintptr_t	test;
 
 	data->dataContent = 2109;
 	PRINT("Value of data content variable before : " << data->dataContent)
-	PRINT("Value of data content variable after : " << deserialize(serialize(data))->dataContent)
+
+	test = serialize(data);
+	PRINT("Value of data content variable after : " << deserialize(test)->dataContent)
 
 	delete data;
 
